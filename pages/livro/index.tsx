a0f0e60216f33275b46c7fffd98a8ff9261e159c
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-01-15 12:15:00 -0300

    new route

---
+++ pages/livro/index.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
+var docInit;
+
+export default function Livro() {
+  const [doc, setdoc] = useState();
+
+  // useEffect(() => {
+  //   document.location.replace("https://go.hotmart.com/D78475892K");
+  // });
+
+  return (
+    <div>
+      <Head>
+        <title>EBOOK DESEJOS E MACILICIAS</title>
+        <meta
+          name="description"
+          content="Página de vendas do livro desejos e malicias"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Marcos Oliveira " />
+      </Head>
+    </div>
+  );
+}
